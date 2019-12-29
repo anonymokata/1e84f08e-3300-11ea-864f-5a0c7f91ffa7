da1588fa631965cc049af54c6e9cdbd5a90aabf8
someone <someone>
2019-12-29 17:57:29 -0500

    Validated that an item can be removed from the inventory.

--- src/main/java/Services/ItemService.java
+++ src/main/java/Services/ItemService.java
@@ -4,28 +4,36 @@ import Entities.Product;
 
 import java.util.HashMap;
 
-//Hosts services that will be used for CRUD operations
+
+
 public class ItemService {
 
     private HashMap<String, Product> productInventory = new HashMap<String, Product>();
 
-    /*
-        Create a product that will be added to the software's inventory list.
-        Under production conditions, product would be stored in a table;
-        for the Kata, Products will be stored in a HashMap that keeps track of inventory
-        for the duration of execution.
-        Products have a name ("Soup"), a pricing method (Per Unit or Per Weight),
-        and a cost (5.99 (Per Weight), 2.00 (Per Unit)).
-     */
-    public Product createItem(String productId, String productPricingMethod, int costOfProductPerPricingMethod) {
+    // Two createProduct methods will be used, as there are two types of Products that require variations in parameters.
+    // One is Per Unit (Can of Soup), and one is per weight (1.00 per pound).
+    // Created products will be stored in the productInventory HashMap.
+    public Product createInventoryProduct(String productId, String productPricingMethod, int costOfProductPerPricingMethod) {
         Product product = new Product(productId, Product.PricingMethod.valueOf(productPricingMethod), costOfProductPerPricingMethod);
         productInventory.put(product.getProductId(), product);
         return product;
     }
 
-    public Product createItem(String productId, String productPricingMethod, int costOfProductPerPricingMethod, int productWeightIfWeighted) {
+    public Product createInventoryProduct(String productId, String productPricingMethod, int costOfProductPerPricingMethod, int productWeightIfWeighted) {
         Product product = new Product(productId, Product.PricingMethod.valueOf(productPricingMethod), costOfProductPerPricingMethod, productWeightIfWeighted);
         productInventory.put(product.getProductId(), product);
         return product;
+    }
+
+    public String removeInventoryProduct(String productId) {
+        if (productInventory.containsKey(productId)) {
+            if(productInventory.size() > 1 ) {
+                productInventory.remove(productId);
+            } else {
+                productInventory = null;
+            }
+        }
+
+        return productId + " removed.";
     }
 }
--- src/test/java/ItemTests.java
+++ src/test/java/ItemTests.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotSame;
 
 @RunWith(JUnit4.class)
 public class ItemTests {
@@ -20,12 +21,18 @@ public class ItemTests {
     @Test
     public void validateThatAUnitBasedProductCanBeCreated() {
         Product product = new Product("Tomato Soup", Product.PricingMethod.Unit, 99);
-        assertEquals(product, itemService.createItem("Tomato Soup", "Unit", 99));
+        assertEquals(product, itemService.createInventoryProduct("Tomato Soup", "Unit", 99));
     }
 
     @Test
     public void validateThatAWeightBasedProductCanBeCreate() {
         Product product = new Product("Carved Turkey", Product.PricingMethod.Weighted, 99, 1200);
-        assertEquals(product, itemService.createItem("Carved Turkey", "Weighted", 99, 1200));
+        assertEquals(product, itemService.createInventoryProduct("Carved Turkey", "Weighted", 99, 1200));
+    }
+
+    @Test
+    public void validateThatAnInventoryProductCanBeRemoved() {
+        itemService.createInventoryProduct("Carved Turkey", "Weighted", 99, 1200);
+        assertEquals("Soup removed.", itemService.removeInventoryProduct("Soup"));
     }
 }
